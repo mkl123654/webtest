a98e5bfb2d4c47c689310392e074771c80f26d32
Lucky <luckymao@maxain.com>
2026-06-04 14:31:28 +0800

    feat: 注册/登录 API + JWT 认证 + /users/me

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, UsersModule],
 })
 export class AppModule {}
